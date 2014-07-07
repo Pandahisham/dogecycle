ebb74a7a733881194bbf4cecaa203a63b811e6e6
erraticmusings <catherine.magicmooie@gmail.com>
2014-07-06 20:53:44 -0400

    Changed GitHub URL.

--- index.php
+++ index.php
@@ -76,7 +76,7 @@ else
 				<div id="cycle-avg"></div>
 			</div>
 		</div>
-		<div class="ourinfo"> wow <a href="https://github.com/itdelatrisu/dogecycle" target="_blank">github</a> such <a href="http://dogeweather.com/" target="_blank">inspired</a></div>
+		<div class="ourinfo"> wow <a href="https://github.com/erraticmusings/dogecycle" target="_blank">github</a> such <a href="http://dogeweather.com/" target="_blank">inspired</a></div>
 		<div id="suchsecrets"><?php include("cycle.php"); ?></div>
 		<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
 		<script>if (typeof window.history.pushState == 'function') { window.history.pushState({}, "Hide", "./"); }</script>
